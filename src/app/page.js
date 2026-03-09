77d9a95112099a2fa0165e174756d78d783a503b
Mahmud Hasan <mahmud.hasan2006@gmail.com>
2026-03-09 22:34:13 +0600

    test page

--- src/app/page.js
+++ src/app/page.js
@@ -11,10 +11,7 @@ export default function Home() {
         <div className="absolute top-[350px] inset-x-0 z-50 flex justify-center px-4">
           <div className="bg-white/10 text-red-500 px-5 py-2.5 rounded-full font-bold backdrop-blur-md uppercase text-[14px] border border-white/20 shadow-2xl">
             <p className="w-full max-w-3xl text-center text-sm md:text-lg font-bold tracking-[0.3em] uppercase opacity-90">
-              lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.
-              lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.
-              lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. 
-            </p>
+              lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate         </p>
           </div>
         </div>
       </div>
